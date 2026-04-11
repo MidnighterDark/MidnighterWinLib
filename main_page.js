fd21134b0302350af125a3e6fc27a1c3141c2964
Midnighter <a01704743@gmail.com>
2026-04-11 22:07:34 +0300

    In this update I update web page only.

--- main_page.js
+++ main_page.js
@@ -26,6 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
 				c.style.animation = "none";
 			}, 250);
 		});
+
+	document.getElementsByTagName("DATE")[0].innerHTML =
+		new Date().getFullYear().toString() + String(" все права защищены.");
 	//Resize
 	document.addEventListener("resize", () => {});
 });
