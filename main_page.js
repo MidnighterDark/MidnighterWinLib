f17a61d94914b0f67e2c745e48361e1b2e677884
Midnighter <a01704743@gmail.com>
2026-05-09 14:35:18 +0300

    Update all!!!

--- main_page.js
+++ main_page.js
@@ -1,12 +1,69 @@
+import * as __MYJSLIB from "./my-library-js-custom/my-library-js-custom.js";
 //On document loaded
 var _current_download_res_select = "n";
 var _domLoad = false;
+var cdw, cdh;
 /**
  * first - news-page's width,
  */
 var _reference_widths = {};
+function resizeFunc() {
+	const cww = Number(
+		window
+			.getComputedStyle(document.documentElement)
+			.width.replace("px", ""),
+	);
+	const cwh = Number(
+		window
+			.getComputedStyle(document.documentElement)
+			.height.replace("px", ""),
+	);
+	document.getElementById("icon-button-settings").style.marginLeft =
+		String(cww - (512 / 100) * 12) + "px";
+	const rbr = document
+		.getElementById("background-right-banner")
+		.getBoundingClientRect();
+	const lbr = document
+		.getElementById("background-left-banner")
+		.getBoundingClientRect();
+	const np = document.getElementById("news-page");
+	const np_style = window.getComputedStyle(np);
+	const npns = document.getElementById("news-scrollbar");
+	const npns_style = window.getComputedStyle(npns);
+	np.style.width = String(rbr.left - lbr.right) + String("px");
+	npns.style.width =
+		String(
+			(Number(np_style.width.replace("px", "")) / _reference_widths[0]) *
+				1050,
+		) + String("px");
+
+	if (rbr.left < lbr.right) {
+		document.getElementById("background-right-banner").style.marginTop =
+			"250px";
+	} else {
+		document.getElementById("background-right-banner").style.marginTop =
+			"0px";
+	}
+	const bkg = document.getElementById("main-background-box-of-download-list");
+	const dle = document.getElementById("download-list");
+	const bkgs = window.getComputedStyle(bkg);
+	const nbkgbw = bkg.getBoundingClientRect().right;
+	if (cww < nbkgbw) {
+		dle.style.marginLeft = String(-300) + "px";
+	} else if (cww > 1060) {
+		dle.style.marginLeft = String(0) + "px";
+	}
+}
 document.addEventListener("DOMContentLoaded", () => {
 	_domLoad = true;
+	cdw = Number(
+		getComputedStyle(document.documentElement).width.replace("px", ""),
+	);
+	cdh = Number(
+		getComputedStyle(document.documentElement).height.replace("px", ""),
+	);
+	document.getElementById("icon-button-settings").style.marginLeft =
+		String(cdw - (512 / 100) * 12) + "px";
 	const news_page = document.getElementById("news-page");
 	document.getElementById("download-list").style.display = "none";
 	document
@@ -31,6 +88,32 @@ document.addEventListener("DOMContentLoaded", () => {
 				c.style.animation = "none";
 			}, 250);
 		});
+	document
+		.getElementById("icon-button-settings")
+		.addEventListener("mousedown", (e) => {
+			setTimeout(() => {
+				const swe = document.getElementById("settings-list");
+				swe.style.display = "block";
+				swe.style.animation = "animopen 0.2s forwards";
+				swe.style.transform =
+					String("translate(") +
+					String(e.pageX - 222) +
+					"px" +
+					String(",") +
+					String(e.pageY - 12) +
+					"px" +
+					String(")");
+			}, 25);
+		});
+	document.addEventListener("mousedown", (e) => {
+		const swe = document.getElementById("settings-list");
+		if (
+			window.getComputedStyle(swe).display != "none" &&
+			!containsInRect(e.pageX, e.pageY, swe.getBoundingClientRect())
+		) {
+			swe.style.display = "none";
+		}
+	});
 	document.getElementsByTagName("COPYRIGHT")[0].innerHTML =
 		new Date().getFullYear().toString() +
 		String(" Midnighter все права защищены.");
@@ -38,39 +121,33 @@ document.addEventListener("DOMContentLoaded", () => {
 	_reference_widths[0] = Number(
 		window.getComputedStyle(news_page).width.replace("px", ""),
 	);
+	resizeFunc();
 	//Resize
 	window.addEventListener("resize", () => {
-		const rbr = document
-			.getElementById("background-right-banner")
-			.getBoundingClientRect();
-		const lbr = document
-			.getElementById("background-left-banner")
-			.getBoundingClientRect();
-		const np = document.getElementById("news-page");
-		const np_style = window.getComputedStyle(np);
-		const npns = document.getElementById("news-scrollbar");
-		const npns_style = window.getComputedStyle(npns);
-		np.style.width = String(rbr.left - lbr.right) + String("px");
-		npns.style.width =
-			String(
-				(Number(np_style.width.replace("px", "")) /
-					_reference_widths[0]) *
-					1050,
-			) + String("px");
-
-		if (rbr.left < lbr.right) {
-			document.getElementById("background-right-banner").style.marginTop =
-				"250px";
-		} else {
-			document.getElementById("background-right-banner").style.marginTop =
-				"0px";
-		}
+		resizeFunc();
 	});
 });
-function checkURL(url) {
-	location.href = url;
+export function checkURL(url) {
+	fetch(url, { method: "HEAD" })
+		.then((response) => {
+			if (response.ok) {
+				location.href = url;
+				console.log("Downloading...");
+			} else {
+				alert("Временно недоступен!!!");
+				console.log(
+					String("Can't find file from: ") +
+						String(url) +
+						String("!!!"),
+				);
+			}
+		})
+		.catch((error) => {
+			console.error("Ошибка при проверке файла:", error);
+		});
 }
-function set_cdrs(newval) {
+window.checkURL = checkURL;
+export function set_cdrs(newval) {
 	if (_domLoad == true) {
 		_current_download_res_select = newval;
 		document.getElementById("_download_list_link_zip_text").innerText =
@@ -82,3 +159,25 @@ function set_cdrs(newval) {
 			"animopen 0.2s forwards";
 	}
 }
+window.set_cdrs = set_cdrs;
+export function __get_current_download_res_select() {
+	return _current_download_res_select;
+}
+window.__get_current_download_res_select = __get_current_download_res_select;
+export function __enable_dark_theme(is) {
+	const bb = document.getElementsByTagName("body")[0];
+	const nsl = document.getElementById("news-scrollbar");
+	const sl = document.getElementById("settings-list-background");
+	let cp;
+	if (is == true) {
+		cp = "rgb(25, 25, 25)";
+	} else if (is == false) {
+		cp = "rgb(250, 250, 250)";
+	} else {
+		console.log("Error parametr 'is' has incorrect value!!!");
+	}
+	bb.style.backgroundColor = cp;
+	nsl.style.backgroundColor = cp;
+	sl.style.backgroundColor = cp;
+}
+window.__enable_dark_theme = __enable_dark_theme;
---
+++ my-library-js-custom/my-library-js-custom.js
@@ -0,0 +1,37 @@
+var data = {};
+data[0] = false;
+data[1] = false;
+document.addEventListener("DOMContentLoaded", () => {
+	data[0] = true;
+});
+document.addEventListener("focusin", () => {
+	data[1] = true;
+});
+document.addEventListener("focusout", () => {
+	data[1] = false;
+});
+class myjslib {
+	checkDOMLoaded() {
+		return Boolean(data[0]);
+	}
+	checkFocus() {
+		return Boolean(data[1]);
+	}
+}
+function includeJsScript(url) {
+	var script = document.createElement("script");
+	script.src = url;
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
+function containsInRect(x, y, rect) {
+	if (
+		x >= rect.left &&
+		x <= rect.right &&
+		y >= rect.top &&
+		y <= rect.bottom
+	) {
+		return true;
+	}
+	return false;
+}
+window.containsInRect = containsInRect;
